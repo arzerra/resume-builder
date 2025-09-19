30e448cae7198fd52f85ad2c650a68817da8d7d2
arzerra <ianclark1506@gmail.com>
2025-09-19 14:40:14 +0800

    resume

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { FaFacebook, FaGithub, FaLinkedin, FaReact } from "react-icons/fa";
 
 function App() {
   const [activeTab, setActiveTab] = useState("personal");
-  const [showModal, setShowModal] = useState(false); // modal state
+  const [showModal, setShowModal] = useState(false); 
 
   const tabs = [
     { id: "personal", label: "Personal" },
@@ -18,51 +18,40 @@ function App() {
     { id: "certificates", label: "Certificate" },
   ];
 
-const [formData, setFormData] = useState({
-  personal: { name: "", email: "", phone: "", location: "" },
-  education: [{ school: "", course: "", year: "" }],
-  experience: [{ company: "", role: "", description: "", year: "" }],
-  skills: [],
-  projects: [{ name: "", description: "", tags: [] }],
-  certificates: [{ name: "", issuer: "", year: "" }],
-});
+  const [formData, setFormData] = useState({
+    personal: { name: "", email: "", phone: "", location: "" },
+    education: [{ school: "", course: "", year: "" }],
+    experience: [{ company: "", role: "", description: "", year: "" }],
+    skills: [],
+    projects: [{ name: "", description: "", tags: [] }],
+    certificates: [{ name: "", issuer: "", year: "" }],
+  });
 
+  const handleChange = (section, field, value, index = null) => {
+    if (section === "personal") {
+      setFormData({
+        ...formData,
+        personal: { ...formData.personal, [field]: value },
+      });
+    } else if (
+      ["education", "experience", "projects", "certificates"].includes(section) &&
+      index !== null
+    ) {
+      const updatedArray = [...formData[section]];
+      updatedArray[index][field] = value;
+      setFormData({ ...formData, [section]: updatedArray });
+    } else {
+      setFormData({ ...formData, [section]: value });
+    }
+  };
 
-const handleChange = (section, field, value, index = null) => {
-  if (section === "personal") {
-    setFormData({
-      ...formData,
-      personal: { ...formData.personal, [field]: value },
-    });
-  } else if (
-    ["education", "experience", "projects", "certificates"].includes(section) &&
-    index !== null
-  ) {
-    const updatedArray = [...formData[section]];
-    updatedArray[index][field] = value;
-    setFormData({ ...formData, [section]: updatedArray });
-  } else {
-    setFormData({ ...formData, [section]: value });
-  }
-};
-
-
-  
   return (
     <>
-      <div className="w-full min-h-screen bg-[#E4E4E4] dark:bg-[#222831]">
-
-
-
+      <div className="w-full min-h-screen bg-[#E4E4E4] dark:bg-[#222831] font-['Quicksand']">
         <div className="max-w-[1300px] w-full mx-auto px-4 sm:px-0">
           
-          <div className="flex flex-row items-center justify-between pt-5 sm:pt-10 pb-5">
-
-            <div className="flex items-center justify-start">
-            <a href="/"><img src="/logo.png" alt="Logo" className="w-20 sm:w-30 cursor-pointer"/></a>
-            </div>
-
-            <div className="flex flex-col items-center justify-center text-center mt-8">
+          <div className="flex flex-row items-center justify-between pt-5 sm:pt-10 pb-0">
+            <div className="flex flex-col items-start justify-start text-start mt-8">
               <h1 className="text-3xl sm:text-5xl font-bold mb-2 dark:text-white">
                 Resume Builder
               </h1>
@@ -70,24 +59,21 @@ const handleChange = (section, field, value, index = null) => {
                 Create. Customize. Land the job.
               </p>
             </div>
-
-            <div className="flex items-center justify-end">
+            <div className="flex items-center justify-start">
               <Darkmode />
+              <a href="/"><img src="/logo.png" alt="Logo" className="w-20 sm:w-25 cursor-pointer"/></a>
             </div>
-
           </div>
 
-
-
-          <div className="grid sm:grid-cols-2 gap-5 items-start">
+          <div className="grid sm:grid-cols-2 gap-5 items-start mt-3 sm:mt-5">
             <div>
-              {/* TAB BAR */}
-              <div className="flex items-center justify-between gap-2 sm:gap-6 dark:border-gray-600 mb-3 overflow-x-auto no-scrollbar">
+              {/* TABS*/}
+              <div className="flex items-center justify-between gap-2 sm:gap-6 mb-3 overflow-x-auto no-scrollbar">
                 {tabs.map((tab) => (
                   <button
                     key={tab.id}
                     onClick={() => setActiveTab(tab.id)}
-                    className={`whitespace-nowrap pb-2 text-sm sm:text-lg font-medium transition-colors ${
+                    className={`whitespace-nowrap pb-2 text-[13px] sm:text-lg font-medium transition-colors ${
                       activeTab === tab.id
                         ? "text-gray-900 border-b-2 dark:border-white border-gray-600 dark:text-white"
                         : "text-gray-600 dark:text-gray-500 hover:text-gray-900 dark:hover:text-white"
@@ -102,7 +88,7 @@ const handleChange = (section, field, value, index = null) => {
               <div className="bg-[#F7F7F7] dark:bg-[#393E46] p-6 rounded-lg shadow-md">
                 {activeTab === "personal" && (
                   <div>
-                    <h2 className="text-xl font-semibold dark:text-white">
+                    <h2 className="text-xl font-semibold dark:text-white mb-2">
                       Personal Information
                     </h2>
                     <p className="mt-2 dark:text-gray-200">Full Name:</p>
@@ -112,7 +98,7 @@ const handleChange = (section, field, value, index = null) => {
                       onChange={(e) =>
                         handleChange("personal", "name", e.target.value)
                       }
-                      className="w-full p-2 rounded"
+                      className="w-full p-2 rounded hover:"
                     />
                     <p className="mt-2 dark:text-gray-200">Phone:</p>
                     <input
@@ -469,14 +455,11 @@ const handleChange = (section, field, value, index = null) => {
                   </div>
                 )}
 
-
-
-
               </div>
             </div>
 
             {/* Right Panel */}
-            <div className="flex flex-col items-center p-0 sm:p-4 w-full">
+            <div className="flex flex-col items-center px-0 sm:px-4 w-full">
               {/* Scrollable preview container */}
               <div className="w-full overflow-x-auto flex justify-center max-h-[550px] overflow-y-auto rounded-md bg-gray-50">
                 <ResumePreview data={formData} />
--- src/components/Darkmode.jsx
+++ src/components/Darkmode.jsx
@@ -33,8 +33,8 @@ const Darkmode = () => {
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
               fill="currentColor"
-              width={32}
-              height={32}
+              width={48}
+              height={48}
             >
               <path
                 fillRule="evenodd"
@@ -48,8 +48,8 @@ const Darkmode = () => {
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
               fill="currentColor"
-              width={32}
-              height={32}
+              width={48}
+              height={48}
             >
               <path d="M12 2.25a.75.75 0 01.75.75v2.25a.75.75 0 01-1.5 0V3a.75.75 0 01.75-.75zM7.5 12a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM18.894 6.166a.75.75 0 00-1.06-1.06l-1.591 1.59a.75.75 0 101.06 1.061l1.591-1.59zM21.75 12a.75.75 0 01-.75.75h-2.25a.75.75 0 010-1.5H21a.75.75 0 01.75.75zM17.834 18.894a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 10-1.061 1.06l1.59 1.591zM12 18a.75.75 0 01.75.75V21a.75.75 0 01-1.5 0v-2.25A.75.75 0 0112 18zM7.758 17.303a.75.75 0 00-1.061-1.06l-1.591 1.59a.75.75 0 001.06 1.061l1.591-1.59zM6 12a.75.75 0 01-.75.75H3a.75.75 0 010-1.5h2.25A.75.75 0 016 12zM6.697 7.757a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 00-1.061 1.06l1.59 1.591z" />
             </svg>
@@ -62,8 +62,8 @@ const Darkmode = () => {
 
 const StyledWrapper = styled.div`
   .toggle {
-    width: 56px;
-    height: 56px;
+    width: 80px;     /* bigger container */
+    height: 80px;    /* bigger container */
     border-radius: 50%;
     display: grid;
     place-items: center;
@@ -102,5 +102,6 @@ const StyledWrapper = styled.div`
     transform: scale(0) rotate(360deg);
   }
 `;
+
 
 export default Darkmode;
